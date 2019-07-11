d7f8036de8f49e09a2674fdbe5e8c816cbf817a7
MessaoudAbdelatif <dzdevsoft@gmail.com>
2019-07-11 02:44:37 +0200

    +Ajout Pagination pb page courrante

--- web/src/main/java/com/lade/app/controller/SiteEscaladeController.java
+++ web/src/main/java/com/lade/app/controller/SiteEscaladeController.java
@@ -2,11 +2,13 @@ package com.lade.app.controller;
 
 import dao.SiteEscaladeDao;
 import entities.SiteEscalade;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SiteEscaladeController {
@@ -15,9 +17,12 @@ public class SiteEscaladeController {
   private SiteEscaladeDao siteEscaladeDao;
 
   @GetMapping("/siteEscalade")
-  public String siteEscalade(Model model) {
-    List<SiteEscalade> siteEscalade = siteEscaladeDao.findAll();
-    model.addAttribute("listSiteEscalade", siteEscalade);
+  public String siteEscalade(Model model,
+      @RequestParam(name = "numPages", defaultValue = "0") int numPages) {
+    Page<SiteEscalade> pagesiteEscalade = siteEscaladeDao.findAll(PageRequest.of(numPages, 5));
+    model.addAttribute("listSiteEscalade", pagesiteEscalade.getContent());
+    model.addAttribute("pages", new int[pagesiteEscalade.getTotalPages()]);
+    model.addAttribute("currentPage", numPages);
 
     return "views/siteEscalade";
   }
